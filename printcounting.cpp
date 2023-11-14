d5da3e2e4f73fd22cc84ce8013c2a3a400628ade
Snehagupta26 <105607723+Snehagupta26@users.noreply.github.com>
2023-11-14 16:09:39 +0530

    Add files via upload

--- printcounting.cpp
+++ printcounting.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
-void counting(int n){
-    //base
-    if(n==0){
+void count(int n){
+    if(n<=0){
         return ;
+        
     }
-    cout<<n<<endl;
-    return counting(n-1);
+  // cout<<n-(n-1);
+    count(n-1);
+    cout<<n;
 }
 int main(){
     int n;
     cin>>n;
-    counting(n);
+    count(n);
     
+
 }
